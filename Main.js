cd2fc5257ebc08f089d672fdae013c3fb1a3149a
Spacetinker <davidrobertmorea@gmail.com>
2026-02-25 09:46:49 -0600

    Update Main.js

--- Main.js
+++ Main.js
@@ -4,16 +4,16 @@ var playerStamina = 10;
 var enemeyStamina = 10;
 var playerMana = 5;
 var enemeyMana = 0;
-var moveSetId = ["q", "b", "h", "bl"];
-var moveSetCost = [1, 2, 4, 2];
-var moveSetAtk = [10, 25, 50, 0];
-var moveSetDef = [0, 0, 0, 5];
-var moveSetEffect = [0, 0, 1];
+var moveSetId = ["q", "b", "h", "bl", "mfb", "msb"];
+var moveSetCost = [1, 2, 6, 2, 0, 0];
+var moveSetMana = [0, 0, 0, 0, 2, 2];
+var moveSetAtk = [10, 20, 30, 0, 15, 35];
+var moveSetDef = [0, 0, 0, 5, 0, 0];
+var moveSetEffect = [0, 0, 1, 0, 2, 1];
 
 function move(type){
-  if(type.substring(0,1)=="m"){
-    if(type.substring(1,2)=="f"){
-      
-    }
+  var selectedMove = moveSetId.index(type)
+  if(moveSetCost[selectedMove] <= playerStamina && moveSetCost[selectedMove] <= playerMana){
+    playerMana
   }
 }
