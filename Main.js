b596bb30dc619126abccde2c59d827095a8364b3
Spacetinker <davidrobertmorea@gmail.com>
2026-02-25 09:01:26 -0600

    Initialize player and enemy stats with new variables

--- Main.js
+++ Main.js
@@ -1,11 +1,19 @@
 var playerHP = 100;
-var enemeyHP = 100;
-var moveSetId = [];
-var moveSetCost = [];
-var moveSetAtk = [];
-var moveSetDef = [];
-var moveSetEffect = [];
+var enemeyHP = 90;
+var playerStamina = 10;
+var enemeyStamina = 10;
+var playerMana = 5;
+var enemeyMana = 0;
+var moveSetId = ["q", "b", "h", "bl"];
+var moveSetCost = [1, 2, 4, 2];
+var moveSetAtk = [10, 25, 50, 0];
+var moveSetDef = [0, 0, 0, 5];
+var moveSetEffect = [0, 0, 1];
 
 function move(type){
-
+  if(type.substring(0,1)=="m"){
+    if(type.substring(1,2)=="f"){
+      
+    }
+  }
 }
